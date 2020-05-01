908094a204dd79b364eb271ab15dde5be8ac913f
Matija Matisic <mmatisic7@gmail.com>
2020-05-01 21:48:43 +0200

    InitialCommit

---
+++ ContosoUniversity/Controllers/StudentsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public StudentsController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        //The async keyword tells the compiler to generate callbacks for parts of the method body
+        //and to automatically create the Task<IActionResult> object that's returned.
+        //The return type Task<IActionResult> represents ongoing work with a result of type IActionResult.
+
+        //Only statements that cause queries or commands to be sent to the database are executed asynchronously.
+        //That includes, for example, ToListAsync, SingleOrDefaultAsync, and SaveChangesAsync. 
+        //It doesn't include, for example, statements that just change an IQueryable,
+        //such as var students = context.Students.Where(s => s.LastName == "Davolio").
+
+        // GET: Students
+        public async Task<IActionResult> Index()
+        {
+            //The await keyword causes the compiler to split the method into two parts.
+            //The first part ends with the operation that's started asynchronously.
+            //The second part is put into a callback method that's called when the operation completes.
+
+            //ToListAsync is the asynchronous version of the ToList extension method.
+            return View(await _context.Students.ToListAsync());
+        }
+
+        // GET: Students/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // GET: Students/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,LastName,FirstMidName,EnrollmentDate")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,LastName,FirstMidName,EnrollmentDate")] Student student)
+        {
+            if (id != student.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(student);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentExists(student.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StudentExists(int id)
+        {
+            return _context.Students.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Permissions;
+using System.Threading;
+
+namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; } //foreign key 
+        public int StudentID { get; set; } //foreign key
+
+        //A grade that's null is different from a zero grade 
+        //null means a grade isn't known or hasn't been assigned yet.
+        public Grade? Grade { get; set; }
+
+        //Navigation properties 
+        //can have only 1 Student entity and only 1 Course entity
+        //1 to many relationship (this is 1) 
+        public Course Course { get; set; } //single course entity
+        public Student Student { get; set; }
+
+    }
+}
