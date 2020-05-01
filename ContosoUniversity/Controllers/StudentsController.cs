fb9cf352d56f4fc119e1d37b3fb37dcd00c38ad8
Matija Matisic <mmatisic7@gmail.com>
2020-05-02 01:57:51 +0200

    Updated Edit

--- ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/Controllers/StudentsController.cs
@@ -72,9 +72,13 @@ namespace ContosoUniversity.Controllers
         // POST: Students/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        // The Bind attribute that the scaffolded code includes on the Create method is one way to protect against overposting in create scenarios.
 
-            //VIEW MODELS
+        // OVERPOSTING Methods
+        //1. BIND - The Bind attribute that the scaffolded code includes on the Create method is one way to protect against overposting in create scenarios.
+        //2. TRYUPDATEMODEL - You can prevent overposting in edit scenarios by reading the entity from the database first and then calling TryUpdateModel, 
+        //      passing in an explicit allowed properties list.
+        //      - primjer u Edit i u RazorPages tutorialu
+        //3. VIEW MODELS
         //An alternative way to prevent overposting that's preferred by many developers
         //is to use view models rather than entity classes with model binding.
         //Include only the properties you want to update in the view model. 
@@ -83,10 +87,12 @@ namespace ContosoUniversity.Controllers
         //Use _context.Entry on the entity instance to set its state to Unchanged,
         //and then set Property("PropertyName").IsModified to true on each entity property that's included in the view model.
         //This method works in both edit and create scenarios.
+
+        // 1. BIND method
         [HttpPost]
-        [ValidateAntiForgeryToken]        
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("EnrollmentDate,FirstMidName,LastName")] Student student)
-        {       
+        {
             //Bind bez ID jer se dodaje automatski, a ne od usera i dodan try catch blok
             try
             {
@@ -107,6 +113,61 @@ namespace ContosoUniversity.Controllers
             return View(student);
         }
 
+        // 2. TryUpdateModelAsync Method
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create(Student student)
+        //{
+        //    if (await TryUpdateModelAsync<Student>(
+        //        student,    // Uses the posted form values
+        //        "student", // Prefix for form value.
+        //        s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate)) //Updates only the properties listed
+        //    {
+        //        try //nisam siguran da li treba? dodano iz Edit metode
+        //        {
+        //            _context.Add(student); //This code adds the Student entity created by the ASP.NET Core MVC model binder
+        //            await _context.SaveChangesAsync(); // save changes
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //        catch (DbUpdateException /* ex */)
+        //        {
+        //            //Log the error (uncomment ex variable name and write a log.)
+        //            ModelState.AddModelError("", "Unable to save changes. " +
+        //                "Try again, and if the problem persists " +
+        //                "see your system administrator.");
+        //        }
+        //    }
+        //    return View(student);
+        //}
+
+        //3. View Model metoda - Mijenjamo i @model u View-u
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create(StudentViewModel studentViewModel) //proslijeđujemo ViewModel
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            var entry = _context.Add(new Student());
+        //            //The SetValues method sets the values of this object by reading values from another PropertyValues object. 
+        //            //SetValues uses property name matching. The view model type doesn't need to be related to the model type,
+        //            //it just needs to have properties that match.
+        //            entry.CurrentValues.SetValues(studentViewModel);
+        //            await _context.SaveChangesAsync(); // save changes
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //        catch (DbUpdateException /*ex*/)
+        //        {
+        //            //Log the error (uncomment ex variable name and write a log.)
+        //            ModelState.AddModelError("", "Unable to save changes. " +
+        //                "Try again, and if the problem persists " +
+        //                "see your system administrator.");
+        //        }
+        //    }
+        //    return View(studentViewModel);
+        //}
+
         // GET: Students/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
@@ -126,37 +187,71 @@ namespace ContosoUniversity.Controllers
         // POST: Students/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+
+        [HttpPost, ActionName("Edit")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,LastName,FirstMidName,EnrollmentDate")] Student student)
+        public async Task<IActionResult> EditPost(int? id)
         {
-            if (id != student.ID)
+            if (id == null)
             {
                 return NotFound();
             }
+            var studentToUpdate = await _context.Students.FirstOrDefaultAsync(s => s.ID == id);
 
-            if (ModelState.IsValid)
+            if (await TryUpdateModelAsync<Student>(
+                studentToUpdate,
+                "",
+                s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate))
             {
                 try
-                {
-                    _context.Update(student);
+                {                    
                     await _context.SaveChangesAsync();
                 }
-                catch (DbUpdateConcurrencyException)
+                catch (DbUpdateException /*ex*/)
                 {
-                    if (!StudentExists(student.ID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    //Log the error (uncomment ex variable name and write a log.)
+                    ModelState.AddModelError("", "Unable to save changes. " +
+                        "Try again, and if the problem persists, " +
+                        "see your system administrator.");
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(student);
+            return View(studentToUpdate);
         }
+
+
+        // 2. BIND - You can use this approach when the web page UI includes all of the fields in the entity and can update any of them.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("ID,LastName,FirstMidName,EnrollmentDate")] Student student)
+        //{
+        //    if (id != student.ID)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(student);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!StudentExists(student.ID))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(student);
+        //}
 
         // GET: Students/Delete/5
         public async Task<IActionResult> Delete(int? id)
