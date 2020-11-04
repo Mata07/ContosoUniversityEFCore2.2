9e55cfba3859fe83949747e33dbac5c085d59038
Mata07 <mmatisic7@gmail.com>
2020-11-04 01:46:00 +0100

    Finish Pagination

--- ContosoUniversity/PaginatedList.cs
+++ ContosoUniversity/PaginatedList.cs
@@ -4,11 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ContosoUniversity.Models
+namespace ContosoUniversity
 {
     public class PaginatedList<T> : List<T>
     {
-        public int PageIndex { get; private set; }
+        public int PageIndex { get; private set; } // Current page read-only
         public int TotalPages { get; private set; }
 
         public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
@@ -16,13 +16,16 @@ namespace ContosoUniversity.Models
             PageIndex = pageIndex;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
 
+            // Adds items to the end of the List<T>
             this.AddRange(items);
         }
 
+        // The properties HasPreviousPage and HasNextPage can be used to enable or disable Previous and Next paging buttons.
         public bool HasPreviousPage
         {
             get
             {
+                // set if current page is greater than 1
                 return (PageIndex > 1);
             }
         }
@@ -35,15 +38,12 @@ namespace ContosoUniversity.Models
             }
         }
 
-        //The CreateAsync method in this code takes page size and page number and applies the appropriate Skip and Take statements to the IQueryable.
-        //When ToListAsync is called on the IQueryable, it will return a List containing only the requested page.
-        //The properties HasPreviousPage and HasNextPage can be used to enable or disable Previous and Next paging buttons.
-        //A CreateAsync method is used instead of a constructor to create the PaginatedList<T> object because constructors can't run asynchronous code.
+        // The CreateAsync method in this code takes page size and page number and applies the appropriate Skip and Take statements to the IQueryable.
+        // A CreateAsync method is used instead of a constructor to create the PaginatedList<T> object because constructors can't run asynchronous code.
         public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
         {
             var count = await source.CountAsync();
             var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
-
             return new PaginatedList<T>(items, count, pageIndex, pageSize);
         }
     }
